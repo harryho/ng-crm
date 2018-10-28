fb5fe35acf6c7b8a051f466621c7a34b5b245117
harryho <harry.ho_long@yahoo.com>
2018-10-29 00:42:06 +1100

    responsive enhancement

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -15,6 +15,10 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { AuthenticationService } from "../_services";
 
+
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+
 /**
  * App Component
  * Entry Component
@@ -30,12 +34,26 @@ export class RootComponent implements OnInit {
   public name = 'Angular MD App';
 
   currentUser: User;
+  isMobile: boolean = false;
 
   constructor(
     public appState: AppState,
     private route: ActivatedRoute,
     private router: Router,
-    public authService: AuthenticationService) { }
+    public authService: AuthenticationService,
+    breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.HandsetLandscape,
+      Breakpoints.HandsetPortrait
+    ]).subscribe(result => {
+      if (result.matches) {
+        // this.activateHandsetLayout();
+        this.isMobile = true;
+      }
+    });
+  }
+
+
 
   public ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser')) || <User>{};
