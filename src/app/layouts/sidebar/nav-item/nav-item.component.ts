519c052a84457dd715fb7123c26903e4008c2768
harryho <harry.ho_long@yahoo.com>
2026-07-15 00:57:59 +1000

    m0: bump ngx-translate (14->18), apexcharts (3->5), ng-apexcharts (1.7->2), ngx-scrollbar (11->19); drop unused TranslateModule; skipLibCheck for apexcharts d.ts bug

--- src/app/layouts/sidebar/nav-item/nav-item.component.ts
+++ src/app/layouts/sidebar/nav-item/nav-item.component.ts
@@ -10,14 +10,13 @@ import { NavItem } from './nav-item';
 import { Router } from '@angular/router';
 import { NavService } from '../../../services/nav.service';
 
-import { TranslateModule } from '@ngx-translate/core';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { MaterialModule } from 'src/app/material.module';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-item',
-  imports: [TranslateModule, TablerIconsModule, MaterialModule, CommonModule],
+  imports: [TablerIconsModule, MaterialModule, CommonModule],
   templateUrl: './nav-item.component.html',
   styleUrls: [],
 })
